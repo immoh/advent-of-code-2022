87133721281de71edb9a9f502fbbf25412dba83e
Immo Heikkinen <immo.heikkinen@gmail.com>
2022-12-10 12:21:50 +0200

    Day 10

---
+++ src/day10.clj
@@ -0,0 +1,38 @@
+(ns day10)
+
+(defn parse-input [input]
+  (map #(clojure.string/split % #" ") (clojure.string/split-lines input)))
+
+(defn execute-instruction [x [op n]]
+  (case op
+    "noop" x
+    "addx" (+ x (parse-long n))))
+
+(defn x-values [instructions]
+  (->> instructions
+       (mapcat (fn [[op :as instruction]]
+                 (if (= "addx" op)
+                   [["noop"] instruction]
+                   [instruction])))
+       (reductions execute-instruction 1)))
+
+(defn signal-strengths [xs]
+  (map (fn [n]
+         (* n (first (drop (dec n) xs))))
+       (range 20 221 40)))
+
+(defn part1 [input]
+  (->> (parse-input input)
+       x-values
+       signal-strengths
+       (reduce +)))
+
+(defn part2 [input]
+  (->> (parse-input input)
+       x-values
+       (map-indexed (fn [i x]
+              (if (<= (dec x) (mod i 40) (inc x))
+                \#
+                \.)))
+       (partition 40)
+       (map (partial apply str))))
